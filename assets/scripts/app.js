91c87b398c2aabcd86156d795cec74774ae8898d
SashaBausheva <abausheva@gmail.com>
2019-06-26 09:48:52 -0400

    Added skeleton for app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,26 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const config = require('./config.js')
+
 $(() => {
-  // your JS code goes here
+  $('#image-uploader').on('submit', function (event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    // FormData is a browser class which allows us to work with multipart/
+    // form-data request
+
+    $.ajax({
+      method: 'POST',
+      url: config.apiUrl + '/uploads',
+      contentType: false, // you use this because StackOverFlow. but basically you just don't want jQuery to interfere, because we've already set the content type
+      processData: false, // same here
+      data: formData // then we can just send it as is because it's already formatted
+    })
+      .then(apiResponse => {
+        $('#image-display').html(`<img src=${apiResponse.upload.url} />`)
+      })
+      .catch(console.log)
+  })
 })
