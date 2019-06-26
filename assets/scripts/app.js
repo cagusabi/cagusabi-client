0a9892d48712ca0c113ce4e05880eee4da2d3f79
gbbenson68 <gbbenson68@gmail.com>
2019-06-26 16:32:08 -0400

    Add auth handlers back
    Added authentication handlers back to app.js.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,29 +6,10 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/events.js')
 const loaderEvents = require('./loader/events.js')
 
 $(() => {
+  authEvents.addHandlers()
   loaderEvents.addHandlers()
 })
-// $(() => {
-//   $('#image-uploader').on('submit', function (event) {
-//     event.preventDefault()
-//
-//     const formData = new FormData(event.target)
-//     // FormData is a browser class which allows us to work with multipart/
-//     // form-data request
-//
-//     $.ajax({
-//       method: 'POST',
-//       url: config.apiUrl + '/uploads',
-//       contentType: false, // you use this because StackOverFlow. but basically you just don't want jQuery to interfere, because we've already set the content type
-//       processData: false, // same here
-//       data: formData // then we can just send it as is because it's already formatted
-//     })
-//       .then(apiResponse => {
-//         $('#image-display').html(`<img src=${apiResponse.upload.url} />`)
-//       })
-//       .catch(console.log)
-//   })
-// })
