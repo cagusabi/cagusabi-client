3a8f1009191f7d888a7ac0abae4fa4e31dc6d54a
wcons <wconstantino@gmail.com>
2019-06-28 13:56:02 -0400

    Add hide/show events to UI with jQuery
    - Landing page will transition to sign-up/sign out on button click
    - Sign-Up/Sign-In buttons switch to Change Password/Sign-Out on click
    - Sign-Out empties content field and return navbars to initial state
    - HTML messaging for user auth actions

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -15,14 +15,25 @@ const onSignUpSuccess = () => {
 
 const onSignUpFailure = () => {
   util.logMessage(`${pkg}.onSignUpFailure()`, 'Sign up failed!')
+  $('#auth-message').html('Failed to sign up')
+  $('#auth-message').fadeIn('slow')
+  $('form').trigger('reset')
+  setTimeout(() => $('#auth-message').fadeOut('slow'), 3000)
 }
 
 const onSignInSuccess = (responseData) => {
   store.user = responseData.user
   util.logMessage(`${pkg}.onSignInSuccess()`, 'Signed in successfully!')
+
   $('#user-message-modal-title').text('Sign In')
   $('#user-message-modal-body').text('Signed in successfully!')
   $('#user-message-modal').modal('show')
+  $('#sign-in').hide()
+  $('#sign-up-btn').hide()
+  $('#sign-in-btn').hide()
+  $('#change-password-btn').show()
+  $('#sign-out-btn').show()
+  $('#image-uploader').show()
   loaderApi.imageIndex()
     .then(loaderUi.onIndexSuccess)
     .catch(loaderUi.failure)
@@ -32,10 +43,23 @@ const onSignInSuccess = (responseData) => {
 
 const onSignInFailure = () => {
   util.logMessage(`${pkg}.onSignInFailure()`, 'Sign in failed!')
+  $('#auth-message').html('Failed to sign up')
+  $('#auth-message').fadeIn('slow')
+  $('form').trigger('reset')
+  setTimeout(() => $('#auth-message').fadeOut('slow'), 3000)
 }
 
 const onSignOutSuccess = () => {
   util.logMessage(`${pkg}.onSignOutSuccess()`, 'Signed out successfully!')
+  $('#auth-message').html('Signed out successfully!')
+  $('#auth-message').fadeIn('slow')
+  setTimeout(() => $('#auth-message').fadeOut('slow'), 3000)
+  $('#sign-up-btn').show()
+  $('#sign-in-btn').show()
+  $('#change-password-btn').hide()
+  $('#sign-out-btn').hide()
+  $('#image-uploader').hide()
+  $('.content').empty()
 }
 
 const onSignOutFailure = () => {
